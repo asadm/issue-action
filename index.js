5a1cad5987fe5455721763fc8c730059132ef5da
Asad Memon <asad.lionpk@gmail.com>
2023-10-22 12:12:24 -0700

    test index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+const { Octokit } = require("@octokit/rest");
+
+async function getIssueAndComments() {
+    const octokit = new Octokit({
+        auth: process.env.GITHUB_TOKEN,
+    });
+
+    const context = {
+        owner: process.env.GITHUB_REPOSITORY.split('/')[0],
+        repo: process.env.GITHUB_REPOSITORY.split('/')[1],
+        issue_number: process.env.GITHUB_EVENT_PATH ? require(process.env.GITHUB_EVENT_PATH).issue.number : undefined,
+    };
+
+    if (!context.issue_number) {
+        console.log("No issue number found!");
+        return;
+    }
+
+    // Get the issue
+    const issue = await octokit.rest.issues.get(context);
+
+    // Log the issue
+    console.log(`Issue by ${issue.data.user.login}: ${issue.data.body}`);
+
+    // Get issue comments
+    const comments = await octokit.rest.issues.listComments(context);
+
+    // Log the comments
+    comments.data.forEach(comment => {
+        console.log(`Comment by ${comment.user.login}: ${comment.body}`);
+    });
+}
+
+getIssueAndComments().catch(error => {
+    console.error(error);
+});
