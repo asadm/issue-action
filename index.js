daa3ebb11b7b6f8290462c7c1665247709615d6d
Asad Memon <asad.lionpk@gmail.com>
2023-10-26 20:23:57 -0700

    ask question method added

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-import { getContextFromIssue } from "./common/githubContext.mjs";
+import { getContextFromIssue,askQuestionOnIssue} from "./common/githubContext.mjs";
 
 getContextFromIssue().then(context => {
     console.log(context);
+    askQuestionOnIssue("How are you doing today?").then(() => {
+        console.log("Asked question!");
+    });
 });
